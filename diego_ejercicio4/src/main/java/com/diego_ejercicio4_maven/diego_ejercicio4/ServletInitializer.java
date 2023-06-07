e687e1415d463af5e9cb842ed72b72cd30e60b3a
DiegoLamana <36438008+DiegoLamana@users.noreply.github.com>
2023-06-07 10:19:50 +0200

    Add files via upload

---
+++ diego_ejercicio4/src/main/java/com/diego_ejercicio4_maven/diego_ejercicio4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.diego_ejercicio4_maven.diego_ejercicio4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DiegoEjercicio4Application.class);
+	}
+
+}
