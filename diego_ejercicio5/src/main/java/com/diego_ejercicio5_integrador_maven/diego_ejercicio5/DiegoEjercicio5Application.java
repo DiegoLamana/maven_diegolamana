295c65b42b2b0dafcf220d6fe82fb68980261f12
DiegoLamana <36438008+DiegoLamana@users.noreply.github.com>
2023-06-07 13:03:56 +0200

    Add files via upload

---
+++ diego_ejercicio5/src/main/java/com/diego_ejercicio5_integrador_maven/diego_ejercicio5/DiegoEjercicio5Application.java
@@ -0,0 +1,13 @@
+package com.diego_ejercicio5_integrador_maven.diego_ejercicio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiegoEjercicio5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiegoEjercicio5Application.class, args);
+	}
+
+}
---
+++ diego_ejercicio5/src/test/java/com/diego_ejercicio5_integrador_maven/diego_ejercicio5/DiegoEjercicio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego_ejercicio5_integrador_maven.diego_ejercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiegoEjercicio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
